d3619291e674f77b2fce0a647e73b805e59437b9
pridato <davidarroyo25052004@gmail.com>
2024-04-26 18:06:25 +0200

    init flask

---
+++ front/src/components/activarLogin.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+
+  let isModalOpen = false;
+  // onMount para cuando el comp. se ha creado
+  import { onMount } from 'svelte';
+  import ModalCliente from './modalCliente.svelte';
+
+  onMount(()=> {
+    const loginButton = document.querySelector('#login')
+
+    loginButton?.addEventListener( 'click', () => {
+      isModalOpen = true
+    })
+  })
+
+</script>
+
+<button id="login" class="rounded-2xl px-4 py-2 bg-[#D93A00]">
+  Iniciar sesión
+</button>
+
+<!-- modal-->
+{#if isModalOpen}
+  <div class="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gray-800 bg-opacity-50">
+    <ModalCliente closeModal={() => isModalOpen = false} />
+  </div>
+{/if}
