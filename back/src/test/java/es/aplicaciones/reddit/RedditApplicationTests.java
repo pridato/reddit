ee22b642c10c28ce6c9992ef79aba0ac2b8114c2
pridato <davidarroyo25052004@gmail.com>
2024-04-26 22:42:52 +0200

    init spring

---
+++ back/src/test/java/es/aplicaciones/reddit/RedditApplicationTests.java
@@ -0,0 +1,13 @@
+package es.aplicaciones.reddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
